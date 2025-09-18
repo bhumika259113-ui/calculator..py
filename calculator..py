46880bcb4cb92f7a1abf9bc2706ca2137bf0a2d6
bhumika259113-ui <bhumika_259113@saitm.ac.in>
2025-09-18 11:47:05 +0530

    Add files via upload

---
+++ calculator..py
@@ -0,0 +1,11 @@
+#userdefine function to make calculator
+def calculator(a,b,operator):
+    if operator=="+":
+       print(a+b)
+    elif operator=="-":
+        print(a-b)
+    elif operator=="*":
+         print(a*b)
+    else:
+         print(a/b)
+calculator(3,7,"-") 
